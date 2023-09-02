9ab298de42e5399744452b902248b90491f17cfd
marco-schneider <marco.schneider452@gmail.com>
2023-09-02 19:03:38 -0300

    Fixing issue with gpio declarations, missing gpio_set_direction for STBY pin

--- TB6612FNG.cpp
+++ TB6612FNG.cpp
@@ -9,13 +9,14 @@ Motor::Motor(gpio_num_t IN1, gpio_num_t IN2, gpio_num_t PWM, gpio_num_t STBY,
   this->IN2 = IN2;
   this->PWM = PWM;
   this->STBY = STBY;
-  this -> unit = unit;
-  this -> timer = timer;
-  this -> iosig = iosig;
-  this -> op = op;
+  this->unit = unit;
+  this->timer = timer;
+  this->iosig = iosig;
+  this->op = op;
 
   gpio_set_direction(IN1, GPIO_MODE_OUTPUT);
   gpio_set_direction(IN2, GPIO_MODE_OUTPUT);
+  gpio_set_direction(STBY, GPIO_MODE_OUTPUT);
   gpio_set_level(IN1, 0);
   gpio_set_level(IN2, 1);
 
