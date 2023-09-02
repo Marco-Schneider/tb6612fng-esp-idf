78438cdda2dde41975df680d367efde9f6aa2ea1
marco-schneider <marco.schneider452@gmail.com>
2023-09-02 16:39:28 -0300

    Re-implementation of the header file

--- include/TB6612FNG.h
+++ include/TB6612FNG.h
@@ -18,36 +18,26 @@
 #define MOTOR10 MCPWM_UNIT_1, MCPWM_TIMER_1, MCPWM1B, MCPWM_OPR_B
 #define MOTOR11 MCPWM_UNIT_1, MCPWM_TIMER_2, MCPWM2B, MCPWM_OPR_B
 
-class MOTOR 
+class Motor 
 {
-    private:
-        // 0 - Stop
-        // 1 - Run
-        //gpio_num_t stby;
-        // A1  0  0 1   1
-        // A2  0  1 0   1
-        //   stop + - brake
-        gpio_num_t a1, a2;
-        // ~100 kHz
-        // GPIO port for pwm out
-        gpio_num_t pwmport;
-        // Unit number of MCPWM (Total 2)
-        mcpwm_unit_t unit;
-        // Timer of MCPWM unit (Total 3)
-        mcpwm_timer_t timer;
-        // IO signal of MCPWM unit (Total 6)
-        mcpwm_io_signals_t iosig;
-        // Operator of MCPWM unit (Total 2)
-        mcpwm_operator_t op;
-    public:
-        MOTOR(gpio_num_t a1, gpio_num_t a2, gpio_num_t pwm,
-            // The following 4 parameters can be simply passed using macro MOTOR0~5
-            mcpwm_unit_t unit, mcpwm_timer_t timer, mcpwm_io_signals_t iosig, mcpwm_operator_t op);
-        ~MOTOR();
-        // Speed -100~100
-        void setspeed(float speed);
-        // The motor will slowly stop because of inertia
-        void stop();
-        // The motor will immediately stop
-        void brake();
+  public:
+    Motor(gpio_num_t IN1, gpio_num_t IN2, gpio_num_t PWM, gpio_num_t STBY,
+      mcpwm_unit_t unit, mcpwm_timer_t timer, mcpwm_io_signals_t iosig, mcpwm_operator_t op);
+
+    ~Motor();
+
+    // Drives the motors, the direction is given by the sign
+    // Expects one value between -100 <= 0 <= 100 (% of duty cycle)
+    void drive(float speed);
+
+    // Sets the STBY pin to low, thus stopping the motors
+    void standby();
+    
+  private:
+    gpio_num_t IN1, IN2, PWM, STBY;
+
+    mcpwm_unit_t unit;
+    mcpwm_timer_t timer;
+    mcpwm_io_signals_t iosig;
+    mcpwm_operator_t op;
 };
