e8fa09f13da60466ea063914e60aa97acc714a49
marco-schneider <marco.schneider452@gmail.com>
2023-09-02 16:57:00 -0300

    Implementing constructor and destructor

--- TB6612FNG.cpp
+++ TB6612FNG.cpp
@@ -1,52 +1,32 @@
 #include "TB6612FNG.h"
 
-MOTOR::MOTOR(gpio_num_t a1, gpio_num_t a2, gpio_num_t pwm,
+#define DEFAULT_FREQUENCY 10000
+
+Motor::Motor(gpio_num_t IN1, gpio_num_t IN2, gpio_num_t PWM, gpio_num_t STBY,
              mcpwm_unit_t unit, mcpwm_timer_t timer, mcpwm_io_signals_t iosig, mcpwm_operator_t op) {
-  this -> a1 = a1;
-  this -> a2 = a2;
-  this -> pwmport = pwm;
-  this -> unit = unit;
-  this -> timer = timer;
-  this -> iosig = iosig;
-  this -> op = op;
-  gpio_set_direction(a1, GPIO_MODE_OUTPUT);
-  gpio_set_direction(a2, GPIO_MODE_OUTPUT);
-  gpio_set_level(a1, 0);
-  gpio_set_level(a2, 1);
-  mcpwm_gpio_init(unit, iosig, pwm);
+  
+  this->IN1 = IN1;
+  this->IN2 = IN2;
+  this->PWM = PWM;
+  this->STBY = STBY;
+
+  gpio_set_direction(IN1, GPIO_MODE_OUTPUT);
+  gpio_set_direction(IN2, GPIO_MODE_OUTPUT);
+
+  mcpwm_gpio_init(unit, iosig, PWM);
+
   mcpwm_config_t config;
-  config.frequency = 10000;
+  config.frequency = DEFAULT_FREQ;
   config.cmpr_a = 0;
   config.cmpr_b = 0;
   config.counter_mode = MCPWM_UP_COUNTER;
   config.duty_mode = MCPWM_DUTY_MODE_0;
+
   mcpwm_init(unit, timer, &config);
 }
 
 MOTOR::~MOTOR() {
-  gpio_set_level(a1, 0);
-  gpio_set_level(a2, 0);
+  gpio_set_level(IN1, 0);
+  gpio_set_level(IN2, 0);
   mcpwm_stop(unit, timer);
-}
-
-void MOTOR::setspeed(float speed) {
-  if(speed >= 0) {
-    gpio_set_level(a1, 0);
-    gpio_set_level(a2, 1);
-    mcpwm_set_duty(unit, timer, op, speed);
-  } else {
-    gpio_set_level(a1, 1);
-    gpio_set_level(a2, 0);
-    mcpwm_set_duty(unit, timer, op, -speed);
-  }
-}
-
-void MOTOR::stop() {
-  gpio_set_level(a1, 0);
-  gpio_set_level(a2, 0);
-}
-
-void MOTOR::brake() {
-  gpio_set_level(a1, 1);
-  gpio_set_level(a2, 1);
 }
